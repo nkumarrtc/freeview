3e381faed817fdd00155d8a466ec8dd4a0ef360a
ejunds <ejunds@gmail.com>
2022-07-10 02:52:33 +0800

    Update and rename run.cmd to run.sh

--- .github/workflows/run.cmd
+++
@@ -1,2 +0,0 @@
-./config.cmd --url https://github.com/MIFNtechnology/freeview --token AYLGUIV2TKVC22WC5GRRPIDCW56XS
- }
---
+++ .github/workflows/run.sh
@@ -0,0 +1,2 @@
+$ ./run.sh --url https://github.com/MIFNtechnology/freeview --token AYLGUIV3RQO42YPDEVENSI3CZHKHK
+ 
