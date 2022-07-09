185c0b118a8a0bccf8d188988cd97f887d64d12f
ejunds <ejunds@gmail.com>
2022-07-10 03:48:49 +0800

    Update buildscript.sh

--- .github/workflows/repo/buildscript.sh
+++ .github/workflows/repo/buildscript.sh
@@ -1,4 +1,4 @@
  # Runs a set of commands using the runners shell
       - name: Run a multi-line script
         run: echo "a19a09f4eda5716e5d48ba86b6b78fc014880c5619b9dba4a059eaf65e131780  actions-runner-linux-x64-2.294.0.tar.gz" | shasum -a 256 -c
-r
+
