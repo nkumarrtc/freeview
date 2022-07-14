6bc07202bfe1c91571b9e0e3ae3cd01185a2b15c
ejunds <ejunds@gmail.com>
2022-07-14 09:45:24 +0800

    Update run.sh

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,2 +1,2 @@
 $ ./run.sh --url https://github.com/MIFNtechnology/freeview --token AYLGUIV3RQO42YPDEVENSI3CZHKHK
- 
+$ ./run.sh --url https://github.com/MIFNtechnology/freeview --token AYLGUISJ4NPB2L3DC24EQQTCZ57MQ
